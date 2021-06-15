a1ccdf343b58e9777fbe80da391ea0fc77b98411
Natalie-Cardot <nataliecardot@gmail.com>
2021-06-15 14:51:52 -0700

    Add filter buttons using manual approach

--- src/Categories.js
+++ src/Categories.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ filterItems }) => {
+  return (
+    <div className="btn-container">
+      {/* Manual approach to filtering not ideal - not in sync with data; would need to add/change category names */}
+      <button className="filter-btn" onClick={() => filterItems('all')}>
+        all
+      </button>
+      <button className="filter-btn" onClick={() => filterItems('breakfast')}>
+        breakfast
+      </button>
+    </div>
+  );
 };
 
 export default Categories;
