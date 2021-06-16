cb28e2c80b90201fcff2d9279f9e18be21bc645c
Natalie-Cardot <nataliecardot@gmail.com>
2021-06-16 12:18:20 -0700

    Filter items/show buttons using dynamic approach

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+// Set object (ES6 feature): create collection of unique values
+// Using spread to convert set object to array (set is iterable)
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
 
-  // Filter menu items using manual approach
   const filterItems = (category) => {
     if (category === 'all') {
       setMenuItems(items);
@@ -24,7 +27,7 @@ function App() {
           <h2>Our Menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
--- src/Categories.js
+++ src/Categories.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ categories, filterItems }) => {
   return (
     <div className="btn-container">
-      {/* Manual approach to filtering not ideal - not in sync with data; would need to add/change category names */}
-      <button className="filter-btn" onClick={() => filterItems('all')}>
-        all
-      </button>
-      <button className="filter-btn" onClick={() => filterItems('breakfast')}>
-        breakfast
-      </button>
+      {categories.map((category, index) => (
+        <button
+          type="button"
+          className="filter-btn"
+          key="index"
+          onClick={() => filterItems(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 };
